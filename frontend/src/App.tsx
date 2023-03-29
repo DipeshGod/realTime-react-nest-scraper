c54621f1f77613c9f2f0cdd60e535044d8688851
Dipesh <dipesh@heubert.com>
2023-03-29 16:28:01 +0545

    play with socket io and react a bit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,69 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { io } from 'socket.io-client';
+
+//By default, the Socket.IO client opens a connection to the server right away. You can prevent this behavior
+//with the autoConnect option:
+//we only want to establish connection when component mounts
+export const socket = io('http://localhost:3000', {
+  autoConnect: false,
+});
 
 function App() {
   const [searchKeyword, setSearchKeyword] = useState('');
   const [error, setError] = useState<null | string>(null);
-  const [message, setMessage] = useState<null | string>(null);
+  const [data, setData] = useState<any[]>([]);
+
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [scrapingStarted, setScrapingStarted] = useState(false);
 
   const handleProductSearch = async () => {
     const body = JSON.stringify({ name: searchKeyword });
 
     try {
-      const response = await fetch('http://localhost:3000/scrape/ebay', {
+      await fetch('http://localhost:3000/scrape/ebay', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body,
       });
-      const data = await response.json();
-      setMessage(data.msg);
     } catch (err: any) {
       setError(err.data.msg);
     }
   };
+
+  useEffect(() => {
+    socket.connect();
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onNewProduct(value: any) {
+      setData((data) => [...data, value]);
+    }
+
+    function handleScrapingStarted(value: boolean) {
+      console.log('yaga aayo');
+      setScrapingStarted(value);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('newProduct', onNewProduct);
+    socket.on('scrapingStarted', handleScrapingStarted);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('newProduct', onNewProduct);
+      socket.off('scrapingStarted', handleScrapingStarted);
+    };
+  }, []);
 
   return (
     <div className="App">
@@ -34,8 +75,9 @@ function App() {
       <br />
       <br />
       <button onClick={handleProductSearch}>Search</button>
+      {JSON.stringify(data)}
       {error && <p style={{ color: 'green' }}>{error}</p>}
-      {message && <p style={{ color: 'red' }}>{message}</p>}
+      {scrapingStarted && <p style={{ color: 'red' }}>Scraping Started</p>}
     </div>
   );
 }
